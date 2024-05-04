15ef2b8862e64bff0d7e3426984e0248c37ebdbb
Larissa Lavinia Alves dos Santos Ferreira <larissa.ferreira@fcamara.com>
2024-05-04 16:00:01 -0300

    feat: add data mock

---
+++ src/data/dataMock.js
@@ -0,0 +1,16 @@
+const dataMock = [
+  {
+    "partOfSpeech": "substantivo masculino",
+    "meanings": [
+      "Conjunto de folhas impressas e reunidas em volume encadernado ou brochado.",
+      "Obra em prosa ou verso, de qualquer extensão, disponibilizada em qualquer meio ou suporte: livro bem escrito; livro eletrônico.",
+      "Divisão menor contida numa obra maior: livro dos salmos.",
+      "[Literatura] Divisão de uma obra, especialmente de uma epopeia.",
+      "Caderno de registro das operações comerciais de; livro-caixa.",
+      "[Figurado] Conjunto de saberes, usado como instrução, ou como fonte de ensino: livro de sabedoria."
+    ],
+    "etymology": "Etimologia (origem da palavra livro). Do latim liber.bri."
+  }
+];
+
+export default dataMock;
